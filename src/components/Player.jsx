7a018f67c10ef334dc48bda5db391b1af64f4cfd
bisso <bisso.jk@gmail.com>
2024-02-29 17:45:09 +0900

    1. onChange -> useRef hook으로 변경

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  const inputPlayerName = useRef();
 
-  const onChangePlayerName = (e) => {
-      setSubmitted(false);
-      setPlayerName(e.target.value);
-  };
+  const [playerName, setPlayerName] = useState(null);
 
-  const onSettingPlayerName = () => {
-      setSubmitted(true);
-  };
+  // const [submitted, setSubmitted] = useState(false);
+  // const onChangePlayerName = (e) => {
+  //     setSubmitted(false);
+  //     setPlayerName(e.target.value);
+  // };
+
+  // const onSettingPlayerName = () => {
+  //     setSubmitted(true);
+  // };
+
+  const handleClick = () => {
+      setPlayerName(inputPlayerName.current.value);
+  }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName: 'unknown entity'}</h2>
+      {/* <h2>Welcome {submitted ? playerName: 'unknown entity'}</h2> */}
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={onChangePlayerName}/>
-        <button onClick={onSettingPlayerName}>Set Name</button>
+        <input ref={inputPlayerName} type="text" />
+        {/* <button onClick={onSettingPlayerName}>Set Name</button> */}
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
